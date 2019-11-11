eb1ea8d08d147085f4ca56c12a56aa06cc3eb916
SaloniBindra11 <55702373+SaloniBindra11@users.noreply.github.com>
2019-11-11 18:24:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,80 +1,74 @@
 import pandas as pd
-import os
-import regex
 import datetime
-from datetime import timedelta
 
-l=["airbus_india_101_20200711T124523.csv","airbus_india_102_20180913T064523.csv","airbus_india_103_20190203T055043.csv","airbus_india_104_20210409T064503.csv"]
-l1=[]
-ee=[]
+
+l=["airbus_india_101_20200711T124523.csv","airbus_india_102_20180913T064523.csv","airbus_india_103_20190203T055043.csv","airbus_india_104_20210409T064503.csv","airbus_india_105_20190409T064503.csv"]
+
+
+
 date=[]
 l2=[]
-e=[]
-tt=[]
-t=[]
 d=[]
+tt1=[]
+tt=[]
 
-def Readcsv(l1):
+# Sorting timestamps
+def getSortedTimestamp(l,tt):
 
   for i in range(len(l)):
-    f=open(l[i],"w+")
-    f.write("hello welcome")
-    contents=l[i]
-    l1.append(contents)
-  #print(l1)
-  f.close()
-  return l1
-
-
-ee=Readcsv(l1)
-print(ee)
-print("\n")
-
-def getSortedTimestamp(ee,t):
-
-  for i in range(len(ee)):
-    s1=regex.split("_",ee[i])
-    s2=list(s1[3].split("."))
-    #print(s2[0])
-    tt1 = pd.Timestamp(s2[0]).strftime('%Y-%m-%d %H:%M:%S')
+    
+    s1=l[i].split('_')
+    s1=list(s1[-1].split('.'))
+    tt1 =pd.Timestamp(s1[0]).strftime('%Y-%m-%d %H:%M:%S')
+    
     tt.append(tt1)
-  #print(tt)
-  t=sorted(tt)
+    
+  tt=sorted(tt)
 
-  return t
+  return tt
 
 print("\n")
-e=getSortedTimestamp(ee,t)
+tt=getSortedTimestamp(l,tt)
 print("sorted timestamps are")
-print(e)
+print(tt)
 
+# Get latest 2 files
 
-def getLatestFiles(e,date,l2):
+def getLatestFiles(tt,date,l2):
   
-  m=len(e)
-  date1=e[m-1]
-  date2=e[m-2]
-  f1=regex.split("-",date1)
-  f2=regex.split("-",date2)
   
+  date1=tt[len(tt)-1]
+  
+  date2=tt[len(tt)-2]
+  #print(date2)
+  f1=date1.split("-")
+  f2=date2.split("-")
+  #print(f1)
+  #print(f2)
   
   for i in range(len(l)):
     if f1[0]+f1[1] in l[i]:
-      #print("file1",l[i])
+      
       l2.append(l[i])
+      
     if f2[0]+f2[1] in l[i]:
-      #print("file2",l[i])
+      
       l2.append(l[i])
+      
+    
   date.append(date1)
   date.append(date2)
 
   return date,l2
 
 print("\n")
-d=getLatestFiles(e,date,l2)
+d=getLatestFiles(tt,date,l2)
 print("latest 2 timestamps are",date)
 print("\n")
 print("latest 2 files are",l2)
+
+
+
 
 
 
